4bf4f46365e6531ffd9549867abe5ce41f254042
Rin Iwai <rin@eyrin.jp>
2025-08-27 13:27:16 +0900

    use mailbox

--- include/catchrobo2025/can/subscription.hpp
+++ include/catchrobo2025/can/subscription.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <atomic>
 #include <cstddef>
 #include <cstdint>
 #include <exception>
@@ -7,13 +8,17 @@
 
 #include <halx/core.hpp>
 #include <halx/peripheral/can.hpp>
+#include <halx/rtos.hpp>
 
 namespace catchrobo2025::can {
 
 template <uint32_t Id, class T> class Subscription {
 public:
   Subscription(halx::peripheral::CanBase &can, uint32_t timeout = 1000) : can_{can}, timeout_{timeout} {
-    auto filter_index = can_.attach_rx_queue({Id, Id, false}, rx_queue_);
+    auto filter_index = can_.attach_rx_filter({Id, Id, false}, [this](const halx::peripheral::CanMessage &msg) {
+      rx_mailbox_.push(msg);
+      last_received_.store(halx::core::get_tick(), std::memory_order_release);
+    });
     if (!filter_index) {
       std::terminate();
     }
@@ -22,25 +27,22 @@ public:
 
   ~Subscription() { can_.detach_rx_filter(filter_index_); }
 
-  std::optional<T> receive() {
-    while (auto msg = rx_queue_.pop(0)) {
-      msg_ = T::deserialize(msg->data);
-      last_received_ = halx::core::get_tick();
+  std::optional<T> peek() {
+    if (static_cast<uint32_t>(halx::core::get_tick() - last_received_.load(std::memory_order_acquire)) > timeout_) {
+      return std::nullopt; // タイムアウト起きてる(デフォルト: 1秒)
     }
-    if (static_cast<uint32_t>(halx::core::get_tick() - last_received_) > timeout_) {
-      return std::nullopt;
+    if (auto msg = rx_mailbox_.peek()) {
+      return T::deserialize(msg->data);
     }
-    return msg_;
+    return std::nullopt;
   }
 
 private:
   halx::peripheral::CanBase &can_;
-  halx::core::RingBuffer<halx::peripheral::CanMessage> rx_queue_{64};
+  halx::rtos::Mailbox<halx::peripheral::CanMessage> rx_mailbox_;
   size_t filter_index_;
   uint32_t timeout_;
-
-  std::optional<T> msg_;
-  uint32_t last_received_ = 0;
+  std::atomic<uint32_t> last_received_{0};
 };
 
 } // namespace catchrobo2025::can
