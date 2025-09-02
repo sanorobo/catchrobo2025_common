79d5772532e9c41df9bede99a767dc5e0fb410b2
koko132232@gmail.com <koko132232@gmail.com>
2025-09-02 16:15:59 +0900

    IDの下位4bitを0にすると他のメッセージと干渉するのでID変更

--- include/catchrobo2025/can.hpp
+++ include/catchrobo2025/can.hpp
@@ -13,22 +13,22 @@ namespace catchrobo2025::can {
 
 #ifdef CATCHROBO2025_MAIN
 // ブロードキャスト
-using ModeOrderPub = Publisher<0xF0, msg::Primitive<uint8_t>>;
+using ModeOrderPub = Publisher<0xF1, msg::Primitive<uint8_t>>;
 
 // end effector
-using EndEffectorOrderPub = Publisher<0x10, msg::EndEffectorOrder>;
+using EndEffectorOrderPub = Publisher<0x11, msg::EndEffectorOrder>;
 
 using EndEffectorModeTelemetrySub = Subscription<0x1F, msg::Primitive<uint8_t>>;
 
 // arm
-using ArmModeOrderPub = Publisher<0x20, msg::Primitive<uint8_t>>;
-using ArmPosOrderPub = Publisher<0x21, msg::Vector2>;
+using ArmModeOrderPub = Publisher<0x21, msg::Primitive<uint8_t>>;
+using ArmPosOrderPub = Publisher<0x22, msg::Vector2>;
 
 using ArmTelemetrySub = Subscription<0x2F, msg::Primitive<uint8_t>>;
 using ArmLocalizationSub = Subscription<0x2E, msg::Vector2>;
 
 // stocker
-using StockerOrderPub = Publisher<0x50, msg::Primitive<uint8_t>>;
+using StockerOrderPub = Publisher<0x51, msg::Primitive<uint8_t>>;
 
 using StockerTelemetrySub = Subscription<0x5F, msg::Primitive<uint8_t>>;
 #endif
@@ -37,9 +37,9 @@ using StockerTelemetrySub = Subscription<0x5F, msg::Primitive<uint8_t>>;
 using ArmTelemetryPub = Publisher<0x2F, msg::Primitive<uint8_t>>;
 using ArmLocalizationPub = Publisher<0x2E, msg::Vector2>;
 
-using ModeOrderSub = Subscription<0xF0, msg::Primitive<uint8_t>>;
-using ArmModeOrderSub = Subscription<0x20, msg::Primitive<uint8_t>>;
-using ArmPosOrderSub =  Subscription<0x21, msg::Vector2>;
+using ModeOrderSub = Subscription<0xF1, msg::Primitive<uint8_t>>;
+using ArmModeOrderSub = Subscription<0x21, msg::Primitive<uint8_t>>;
+using ArmPosOrderSub =  Subscription<0x22, msg::Vector2>;
 #endif
 
 #ifdef CATCHROBO2025_END_EFFECTOR
