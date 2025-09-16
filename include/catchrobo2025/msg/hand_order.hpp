ec5e4fa5176fa4f0ad9494b206bacc2753074b3d
tat0430 <tateiba0430@icloud.com>
2025-09-17 06:57:16 +0900

    add_enum

--- include/catchrobo2025/msg/hand_order.hpp
+++ include/catchrobo2025/msg/hand_order.hpp
@@ -8,10 +8,11 @@ enum class HandOrder : uint8_t {
   START = 0b00000001,
   IDLE = 0b00000010,
   ESCAPE = 0b00000100,
-  POS0 = 0b00001000,
+  PUSH = 0b00001000,
   POS1 = 0b00010000,
   POS2 = 0b00100000,
   POS3 = 0b01000000,
+  BACK = 0b10000000,
 };
 
 }
