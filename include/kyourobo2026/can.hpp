8513087241a0ca8565b4f492ab3d0bf4d2c65692
Rin Iwai <rin@eyrin.jp>
2026-02-21 23:18:26 +0900

    共ロボ用メッセージ追加

---
+++ include/kyourobo2026/can.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "catchrobo2025/project_name.hpp"
+
+#include "catchrobo2025/can/publisher.hpp"
+#include "catchrobo2025/can/subscription.hpp"
+
+#include "catchrobo2025/msg/enum_set.hpp"
+#include "catchrobo2025/msg/primitive.hpp"
+
+#include "msg/open_close_state.hpp"
+
+namespace kyourobo2025::can {
+
+using catchrobo2025::can::Publisher;
+using catchrobo2025::can::Subscription;
+
+#ifdef KYOUROBO2026_ASHI
+using Spiral1Pub = Publisher<0x11, catchrobo2025::msg::Primitive<float>>;
+using Spiral2Pub = Publisher<0x12, catchrobo2025::msg::Primitive<float>>;
+using Spiral3Pub = Publisher<0x13, catchrobo2025::msg::Primitive<float>>;
+using OpenClosePub = Publisher<0x14, catchrobo2025::msg::EnumSet<msg::OpenCloseState>>;
+
+using UpDownPub = Publisher<0x15, catchrobo2025::msg::Primitive<float>>;
+using TurretYawPub = Publisher<0x16, catchrobo2025::msg::Primitive<float>>;
+#endif
+
+#ifdef KYOUROBO2026_ATAMA
+using Spiral1Sub = Subscription<0x11, catchrobo2025::msg::Primitive<float>>;
+using Spiral2Sub = Subscription<0x12, catchrobo2025::msg::Primitive<float>>;
+using Spiral3Sub = Subscription<0x13, catchrobo2025::msg::Primitive<float>>;
+using OpenCloseSub = Subscription<0x14, catchrobo2025::msg::EnumSet<msg::OpenCloseState>>;
+
+using UpDownSub = Subscription<0x15, catchrobo2025::msg::Primitive<float>>;
+using TurretYawSub = Subscription<0x16, catchrobo2025::msg::Primitive<float>>;
+#endif
+
+} // namespace kyourobo2025::can
---
+++ include/kyourobo2026/msg/open_close_state.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+namespace kyourobo2025::msg {
+
+// 頭開閉状態
+enum class OpenCloseState {
+  OPEN,
+  CLOSE,
+};
+
+} // namespace kyourobo2025::msg
