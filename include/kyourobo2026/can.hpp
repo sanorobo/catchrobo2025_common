c8db844ece99b0aa1cf1e87325bfe1d270a25ee6
ryuoh0514 <ryusama.dragon.monky.77@gmail.com>
2026-03-04 21:08:14 +0900

    can-ashi変更

--- include/kyourobo2026/can.hpp
+++ include/kyourobo2026/can.hpp
@@ -17,13 +17,17 @@ using catchrobo2025::can::Publisher;
 using catchrobo2025::can::Subscription;
 
 #ifdef KYOUROBO2026_ASHI
-using OpenClosePub = Publisher<0x11, catchrobo2025::msg::Primitive<msg::OpenCloseState>>;
-using UpDownPub = Publisher<0x12, catchrobo2025::msg::Primitive<float>>;
-using TurretYawPub = Publisher<0x13, catchrobo2025::msg::Primitive<float>>;
+using OpenCloseSub = Publisher<0x11, catchrobo2025::msg::Primitive<uint8_t>>;
+using UpDownSub = Publisher<0x12, catchrobo2025::msg::Primitive<uint8_t>>;
+using TurretYawSub = Publisher<0x13, catchrobo2025::msg::Primitive<uint8_t>>;
 
-using Spiral1Pub = Publisher<0x14, catchrobo2025::msg::Primitive<float>>;
-using Spiral2Pub = Publisher<0x15, catchrobo2025::msg::Primitive<float>>;
-using Spiral3Pub = Publisher<0x16, catchrobo2025::msg::Primitive<float>>;
+using Spiral1Sub = Publisher<0x14, catchrobo2025::msg::Primitive<uint8_t>>;
+using Spiral2Sub = Publisher<0x15, catchrobo2025::msg::Primitive<uint8_t>>;
+using Spiral3Sub = Publisher<0x16, catchrobo2025::msg::Primitive<uint8_t>>;
+
+using Entire = Publisher<0x17, catchrobo2025::msg::Primitive<uint8_t>>;
+
+using Pub = Subscription<0x20, catchrobo2025::msg::Primitive<uint16_t>>;
 
 using FeetechPositionPub = Publisher<0x31, msg::ServoPosition>;
 using FeetechPositionSub = Subscription<0x32, msg::ServoPosition>;
@@ -31,13 +35,17 @@ using PwmServoPositionPub = Publisher<0x33, msg::ServoPosition>;
 #endif
 
 #ifdef KYOUROBO2026_ATAMA
-using OpenCloseSub = Subscription<0x11, catchrobo2025::msg::Primitive<msg::OpenCloseState>>;
-using UpDownSub = Subscription<0x12, catchrobo2025::msg::Primitive<float>>;
-using TurretYawSub = Subscription<0x13, catchrobo2025::msg::Primitive<float>>;
+using OpenCloseSub = Subscription<0x11, catchrobo2025::msg::Primitive<uint8_t>>;
+using UpDownSub = Subscription<0x12, catchrobo2025::msg::Primitive<uint8_t>>;
+using TurretYawSub = Subscription<0x13, catchrobo2025::msg::Primitive<uint8_t>>;
 
-using Spiral1Sub = Subscription<0x14, catchrobo2025::msg::Primitive<float>>;
-using Spiral2Sub = Subscription<0x15, catchrobo2025::msg::Primitive<float>>;
-using Spiral3Sub = Subscription<0x16, catchrobo2025::msg::Primitive<float>>;
+using Spiral1Sub = Subscription<0x14, catchrobo2025::msg::Primitive<uint8_t>>;
+using Spiral2Sub = Subscription<0x15, catchrobo2025::msg::Primitive<uint8_t>>;
+using Spiral3Sub = Subscription<0x16, catchrobo2025::msg::Primitive<uint8_t>>;
+
+using Entire = Subscription<0x17, catchrobo2025::msg::Primitive<uint8_t>>;
+
+using Pub = Publisher<0x20, catchrobo2025::msg::Primitive<uint16_t>>;
 #endif
 
 #ifdef KYOUROBO2026_SERVO
